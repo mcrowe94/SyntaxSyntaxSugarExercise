7b6727c0c3d6ca7f632639f773e3f750061a643f
Monica Crowe <mcrowe9194@gmail.com>
2022-04-16 15:54:34 -0500

    Add project files.

---
+++ SyntaxSyntaxSugarExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SyntaxSyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;   //changed to inferred typing
+            string responce;
+
+            if (answer < 9)
+            {
+                responce = $"{answer} is less than nine";  //string interpolation
+            }
+            else
+            {
+                responce = answer + "is greater than or equal to nine";
+            }            
+            var inline = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
+            Console.WriteLine(responce);
+            Console.WriteLine($"this is the inline if: {inline}");
+
+            
+
+
+
+
+
+        }
+
+
+    }
+}
