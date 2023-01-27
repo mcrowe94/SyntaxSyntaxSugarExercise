db66f17e312f58aaff6ce79e6ad7ea89106222ad
Monica Crowe <mcrowe9194@gmail.com>
2023-01-27 11:06:18 -0600

    Changed spelling

--- SyntaxSyntaxSugarExercise/Program.cs
+++ SyntaxSyntaxSugarExercise/Program.cs
@@ -7,18 +7,18 @@ namespace SyntaxSyntaxSugarExercise
         static void Main(string[] args)
         {
             var answer = 4;   //changed to inferred typing
-            string responce;
+            string response;
 
             if (answer < 9)
             {
-                responce = $"{answer} is less than nine";  //string interpolation
+                response = $"{answer} is less than nine";  //string interpolation
             }
             else
             {
-                responce = answer + "is greater than or equal to nine";
+                response = answer + "is greater than or equal to nine";
             }            
             var inline = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
-            Console.WriteLine(responce);
+            Console.WriteLine(response);
             Console.WriteLine($"this is the inline if: {inline}");
 
             
